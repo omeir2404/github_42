507056b56e74a4727987cac55983083b8ac5c5a5
Omeir Mohamed Omar Haroon <oharoon@c1r15s6.42lisboa.com>
2022-11-07 20:58:33 +0000

    finishing additional functions

--- 42_curcus/libft/libft.h
+++ 42_curcus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: oharoon <oharoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:31:36 by oharoon           #+#    #+#             */
-/*   Updated: 2022/11/07 18:41:09 by oharoon          ###   ########.fr       */
+/*   Updated: 2022/11/07 20:53:05 by oharoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,9 @@ char			**ft_split(char const *s, char c);
 char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
 
 #endif
