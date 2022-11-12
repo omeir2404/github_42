65ff8ddfed31e1a28ac0353a28232c05c3ff1149
omeir2404 <80172906+omeir2404@users.noreply.github.com>
2022-11-12 15:49:13 +0000

    random commit

--- 42_curcus/libft/libft.h
+++ 42_curcus/libft/libft.h
@@ -13,6 +13,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+typedef int	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
