7d69e68e2bd19dd578cf6401826fa2fdef26fba3
Omeir Mohamed Omar Haroon <oharoon@c1r15s2.42lisboa.com>
2023-01-09 17:05:54 +0000

    adding multiple args to stack.a

--- 42_curcus/push_swap/push_swap.c
+++ 42_curcus/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: oharoon <oharoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/24 12:48:49 by oharoon           #+#    #+#             */
-/*   Updated: 2023/01/07 14:28:09 by oharoon          ###   ########.fr       */
+/*   Updated: 2023/01/09 17:02:29 by oharoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,22 @@ int	main(int ac, char **av)
 {
 	t_stack	stack;
 	int		count_args;
+	t_list	*temp;
 
-	count_args = 0;
+	count_args = 1;
 	stack.a = NULL;
 	stack.b = NULL;
 	if (ac == 1)
 		exit(0);
 	if (check_input() == 1)
-		while (count_args < ac)
-			stack.a = add_to_a(av[count_args++]);
-	printf("%i", stack.a->value);
+		stack.a = add_to_a(av[count_args++]);
+	while (count_args < ac)
+		ft_lstadd_back(&stack.a, add_to_a(av[count_args++]));
+	temp = stack.a;
+	while (temp != NULL)
+	{
+		printf("%i ", temp->value);
+		temp = temp->next;
+	}
 	free (stack.a);
 }
